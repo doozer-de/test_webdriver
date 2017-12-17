47e94f4fadd58f3dfdeff0f4937f8f4cde95f91c
Eric Schneller <es@doozer.de>
2017-12-17 10:54:05 +0100

    improve(example) - remove example package implementation to prevent analysis error

--- example/test/login_test.dart
+++ example/test/login_test.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+
 import 'package:test_webdriver/test_webdriver.dart';
-import 'package:example/login_po.dart';
+import 'package:pageloader/objects.dart';
 
 void main() {
   group('login', suite(() {
@@ -25,4 +27,43 @@ void main() {
           await po.notExists.call();
         }, screenshotName: 'not_exists'));
   }));
+}
+
+@ByClass('login-page')
+class LoginPO {
+  @ByTagName('form')
+  LoginFormPO form;
+
+  @ById('alink')
+  PageLoaderElement _link;
+
+  Future navigate() => _link.click();
+}
+
+class LoginFormPO {
+  @ById('username')
+  PageLoaderElement _username;
+
+  @ById('password')
+  PageLoaderElement _password;
+
+  @FirstByCss('input[type="submit"]')
+  PageLoaderElement _submit;
+
+  Future<Null> login(String username, String password) async {
+    await _username.type(username);
+    await _password.type(password);
+    await _submit.click();
+  }
+}
+
+@ById('another')
+class AnotherPO {
+  @ByTagName('p')
+  PageLoaderElement _content;
+
+  @ById('notExists')
+  Lazy<PageLoaderElement> notExists;
+
+  Future<String> get content => _content.innerText;
 }
